22e90f9332952e3db59dc9d976250797fc84c65f
Aman8593 <amanwadgaonkar8593@gmail.com>
2024-07-02 14:44:57 +0530

    First Commit

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import './App.css'; // Assuming you have some CSS styles in App.css
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+        Containerization is a lightweight alternative to full machine virtualization that involves encapsulating an application in a container with its own operating environment. This allows the application to run on any computing environment, whether physical or virtual, without changes.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
